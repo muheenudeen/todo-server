87ebf1e64bbb1c8d840d12b590c282fb8084351f
muheenudeen <muheenudeen313@gmail.com>
2024-11-04 18:12:16 +0530

    server-page-edit

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { userRouter } from './routes/userRouter.js'
 import dotenv from 'dotenv';
-
+import cors from 'cors'
 const app=express()
 app.use(express.json())
 const PORT=3004
-
+app.use(cors())
 dotenv.config()
 app.use('/api/user',userRouter)
 
@@ -16,7 +16,6 @@ app.listen(PORT,()=>{
 })
 
 main()
-
 async function main(){
  await mongoose.connect('mongodb+srv://muheenudeen313:KBggrvsPnq0A8ejQ@cluster0.hhyge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 }
